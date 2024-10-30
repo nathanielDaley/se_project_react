51fac205eae4dd85b6a3a6543d44909e6dfe7ecc
Nathaniel Daley <ndaley64@gmail.com>
2024-10-30 17:45:23 -0500

    add and format item cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "../Main/Main.jsx";
 import Header from "../Header/Header.jsx";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+
   return (
     <div className="app">
       <div className="app__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
