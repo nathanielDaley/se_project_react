282663c5b924395056a54b638e5bfb280b982e08
Nathaniel Daley <ndaley64@gmail.com>
2024-10-29 22:25:16 -0500

    create header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "../Header/Header.jsx";
 
 function App() {
-  return <div className="app">Hello</div>;
+  return (
+    <div className="app">
+      <div className="app__content">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
