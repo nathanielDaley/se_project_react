099d4c72d1f159900937e65d96cb008d30f48bd0
Nathaniel Daley <ndaley64@gmail.com>
2024-11-14 22:29:54 -0600

    add sliding transition to developer link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__name">Developed by Nathaniel Daley</p>
+      <a
+        href="https://github.com/nathanielDaley/se_project_react"
+        className="footer__link"
+      >
+        <span
+          data-content="Developed by Nathaniel Daley"
+          aria-hidden="true"
+          className="footer__link-transition"
+        ></span>
+        Developed by Nathaniel Daley
+      </a>
       <p className="footer__date">{new Date().getFullYear()}</p>
     </footer>
   );
