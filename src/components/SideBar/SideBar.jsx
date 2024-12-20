72d9d0e36326d88581e89963974863cb70f065f1
Nathaniel Daley <ndaley64@gmail.com>
2024-12-20 13:28:27 -0600

    use user info for profile sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,20 @@
 import "./SideBar.css";
 import avatar from "../../assets/avatar-default.png";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function Sidebar() {
+  const { currentUser } = useContext(CurrentUserContext);
+
   return (
     <div className="sidebar">
       <div className="sidebar__user-container">
-        <img src={avatar} alt="Terrence Tegegne" className="sidebar__avatar" />
-        <p className="sidebar__username">Terrence Tegegne</p>
+        <img
+          src={currentUser.avatar}
+          alt={currentUser.name}
+          className="sidebar__avatar"
+        />
+        <p className="sidebar__username">{currentUser.name}</p>
       </div>
     </div>
   );
